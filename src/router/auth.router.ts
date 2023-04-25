17d856105e47fe75638147f65f21d1437ba1e7eb
OpeOginni <brightoginni123@gmail.com>
2023-04-25 19:28:02 +0100

    Renamned Endpoint

--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -6,6 +6,5 @@ export default (router: express.Router) => {
 
     router.post('/auth/login', login);
     router.post('/auth/register', register);
-    router.post('/auth/refRegistration', referedRegistration);
-
+    router.post('/auth/refregistration', referedRegistration);
 };
