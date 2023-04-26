21b2b76919232d46f297a8799798f89e753db9d4
OpeOginni <brightoginni123@gmail.com>
2023-04-26 09:15:09 +0100

    RefRegistration

--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -6,5 +6,6 @@ export default (router: express.Router) => {
 
     router.post('/auth/login', login);
     router.post('/auth/register', register);
-    router.post('/auth/refregistration', referedRegistration);
+    // router.post('/auth/refregistration', referedRegistration);
+    router.post('/auth/refRegistration', referedRegistration);
 };
