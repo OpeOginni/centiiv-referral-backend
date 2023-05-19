0208e6d5b82a3640c817aec5a514db36436b4803
OpeOginni <brightoginni123@gmail.com>
2023-05-19 23:19:11 +0100

    Added Delete Users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { getUserById } from '../model/users'
+import { getUserById, deleteAllUsers } from '../model/users'
 
 export const getUser = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
@@ -30,6 +30,18 @@ export const updateUser = async (req: express.Request, res: express.Response, ne
         return res.status(200).json({ success: true, user })
 
     } catch (error) {
+        console.log(error);
+        return res.sendStatus(400)
+    }
+}
+
+export const deleteUsers = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+
+        await deleteAllUsers();
+        return res.sendStatus(200)
+    }
+    catch (error) {
         console.log(error);
         return res.sendStatus(400)
     }
